782bc1d116d3648995bcbfa12c7bc7c60c3c2443
ontama154 <onchanvvv@gmail.com>
2022-04-18 13:54:06 +0900

    Initial Commit

---
+++ swiftUI_100fungo/swiftUI_100fungoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_100fungoApp.swift
+//  swiftUI_100fungo
+//
+//  Created by tomoyo_kageyama on 2022/04/18.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_100fungoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
