b9245ccdf9d5ba1cad39eb254430bc52f58a21f0
ontama154 <onchanvvv@gmail.com>
2022-04-19 10:13:10 +0900

    first commit

---
+++ swiftUI_100fungo/nock3.swift
@@ -0,0 +1,20 @@
+//
+//  nock3.swift
+//  swiftUI_100fungo
+//
+//  Created by tomoyo_kageyama on 2022/04/18.
+//
+
+import SwiftUI
+
+struct nock3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct nock3_Previews: PreviewProvider {
+    static var previews: some View {
+        nock3()
+    }
+}
---
+++ swiftUI_100fungo/nock4.swift
@@ -0,0 +1,20 @@
+//
+//  nock4.swift
+//  swiftUI_100fungo
+//
+//  Created by tomoyo_kageyama on 2022/04/18.
+//
+
+import SwiftUI
+
+struct nock4: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct nock4_Previews: PreviewProvider {
+    static var previews: some View {
+        nock4()
+    }
+}
