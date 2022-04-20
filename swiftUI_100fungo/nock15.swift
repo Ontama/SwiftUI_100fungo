4220126256e63f8c295ee676222bd39b3bc12f9d
ontama154 <onchanvvv@gmail.com>
2022-04-20 09:44:20 +0900

    nock15

---
+++ swiftUI_100fungo/nock15.swift
@@ -0,0 +1,29 @@
+//
+//  nock15.swift
+//  swiftUI_100fungo
+//
+//  Created by tomoyo_kageyama on 2022/04/20.
+//
+
+import SwiftUI
+
+struct nock15: View {
+    var body: some View {
+        (Text("I ")
+         + Text("love ")
+            .foregroundColor(.red)
+        + Text("Snorlax ")
+            .foregroundColor(.black)
+            .font(.system(size: 20).bold())
+        + Text("\n")
+        + Text("Snorlax is most cute & cool& powerful Pokemon.")
+            ).lineSpacing(4)
+    }
+}
+
+struct nock15_Previews: PreviewProvider {
+    static var previews: some View {
+        nock15()
+            .previewLayout(.sizeThatFits)
+    }
+}
